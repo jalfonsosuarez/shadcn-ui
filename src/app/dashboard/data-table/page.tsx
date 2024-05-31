c5335c8a9a80b70b1d0a47256117c80c62e4f6b5
= <=>
2024-05-31 19:25:13 +0200

    Section 4. Fisrt table-data. (Part 2)

--- src/app/dashboard/data-table/page.tsx
+++ src/app/dashboard/data-table/page.tsx
@@ -1,4 +1,6 @@
 import { payments } from "@/data/payments.data";
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
 
 
 async function fetchData() {
@@ -11,9 +13,7 @@ export default async function Page() {
 
   return (
     <div>
-      <pre>
-        {JSON.stringify(data, null, 2)}
-      </pre>
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
