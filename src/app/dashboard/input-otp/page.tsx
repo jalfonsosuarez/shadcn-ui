3b91d7e112ffbf2c65bc47627d2e5ea135665dcc
= <=>
2024-05-31 14:56:17 +0200

    Section 3. Next 4 components (part 4)

---
+++ src/app/dashboard/input-otp/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
+import { useState } from "react";
+
+export default function Page() {
+
+  const [value, setValue] = useState("")
+
+
+  return (
+    <div className="flex flex-col justify-center items-center h-[250px]">
+      <InputOTP
+        maxLength={9}
+        value={value}
+        onChange={(value) => setValue(value)}
+      >
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={6} />
+          <InputOTPSlot index={7} />
+          <InputOTPSlot index={8} />
+        </InputOTPGroup>
+      </InputOTP>
+      <div className="mt-5 text-center text-sm">
+        {value === "" ? (
+          <>Enter your one-time password.</>
+        ) : (
+          <>You entered: {value}</>
+        )}
+      </div>
+    </div>
+  );
+}
